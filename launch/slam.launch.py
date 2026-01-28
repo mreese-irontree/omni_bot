ad6fdb785714be1fc87c10e6f77e83757d867d23
mreese-irontree <mreese@irontreesolutions.com>
2026-01-28 11:14:17 -0800

    Made slam_toolbox go active in launch file now

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -5,12 +5,15 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    # Bring up robot (URDF, lidar, cmdvel bridges, etc.)
     launch_robot = IncludeLaunchDescription(
         PythonLaunchDescriptionSource('/home/matt/omni_bot_ws/src/omni_bot/launch/launch_robot.launch.py'),
     )
 
+    # slam_toolbox params (FULL PATH)
     slam_params = '/home/matt/omni_bot_ws/src/omni_bot/config/slam_toolbox.yaml'
-    slam_node = Node(
+
+    slam_toolbox_node = Node(
         package='slam_toolbox',
         executable='sync_slam_toolbox_node',
         name='slam_toolbox',
@@ -18,4 +21,21 @@ def generate_launch_description():
         parameters=[slam_params],
     )
 
-    return LaunchDescription([launch_robot, slam_node])
+    # Lifecycle manager to configure+activate slam_toolbox
+    lifecycle_manager = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_slam',
+        output='screen',
+        parameters=[{
+            'use_sim_time': False,
+            'autostart': True,
+            'node_names': ['slam_toolbox'],
+        }],
+    )
+
+    return LaunchDescription([
+        launch_robot,
+        slam_toolbox_node,
+        lifecycle_manager,
+    ])
